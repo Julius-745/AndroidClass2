8ea70d84a2ee5c78cd70589c63c42b7bca73c519
Julius-745 <juliusagung123@gmail.com>
2021-09-09 17:10:47 +0700

    base component

---
+++ app/src/main/java/com/julius745/AndroidClass2/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.julius745.AndroidClass2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Context konteks = getApplicationContext();
+        CharSequence kata = "Berhasil!";
+        int durasi = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(konteks,kata,durasi);
+        toast.show();
+
+        WebView web = (WebView) findViewById(R.id.wv);
+        web.loadUrl("https://www.trunojoyo.ac.id/");
+
+    }
+}
---
+++ app/src/main/java/com/julius745/AndroidClass2/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.julius745.AndroidClass2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_LENGTH = 5000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(SplashScreen.this,SecondActivity.class);
+                SplashScreen.this.startActivity(main);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_LENGTH);
+
+    }
+}
