629e43cfa08b7445221670e04642cc288970838e
Julius-745 <juliusagung123@gmail.com>
2021-09-22 19:39:52 +0700

    menu fix w/out content

--- app/src/main/java/com/activity/AndroidClass2/MainActivity.java
+++ app/src/main/java/com/activity/AndroidClass2/MainActivity.java
@@ -1,21 +1,12 @@
 package com.activity.AndroidClass2;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -44,21 +35,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-    public boolean onCreateOptionMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    public boolean onOptionItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.settings) {
-            startActivity(new Intent(this, Settings.class));
-        } else if (item.getItemId() == R.id.about) {
-            startActivity(new Intent(this, About.class));
-        } else {
-            startActivity(new Intent(this, Help.class));
-        }
-        return true;
     }
 }
--- app/src/main/java/com/activity/AndroidClass2/SecondActivity.java
+++ app/src/main/java/com/activity/AndroidClass2/SecondActivity.java
@@ -3,7 +3,11 @@ package com.activity.AndroidClass2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.Toast;
 
@@ -24,5 +28,30 @@ public class SecondActivity extends AppCompatActivity {
         WebView web = (WebView) findViewById(R.id.wv);
         web.loadUrl("https://www.trunojoyo.ac.id/");
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.about:
+                Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, About.class));
+                return true;
+            case R.id.settings:
+                Toast.makeText(getApplicationContext(), "Help", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, Help.class));
+                return true;
+            case R.id.logout:
+                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
