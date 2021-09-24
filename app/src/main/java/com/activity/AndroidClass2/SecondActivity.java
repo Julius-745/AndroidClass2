808da8e518db27969e02c476210cc67bf946a74b
Julius-745 <juliusagung123@gmail.com>
2021-09-24 20:42:27 +0700

    explicit intent done

--- app/src/main/java/com/activity/AndroidClass2/SecondActivity.java
+++ app/src/main/java/com/activity/AndroidClass2/SecondActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
@@ -24,9 +28,40 @@ public class SecondActivity extends AppCompatActivity {
         int durasi = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(konteks,kata,durasi);
         toast.show();
-        // ambil webview trus load halaman UTM
-        WebView web = (WebView) findViewById(R.id.wv);
-        web.loadUrl("https://www.trunojoyo.ac.id/");
+
+        RelativeLayout web1 = (RelativeLayout) findViewById(R.id.btn1);
+        RelativeLayout web2 = (RelativeLayout) findViewById(R.id.btn2);
+        RelativeLayout web3 = (RelativeLayout) findViewById(R.id.btn3);
+
+        web1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    String url = "https://www.cnnindonesia.com/";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+        });
+
+        web2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.kompas.com/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        web3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.detik.com/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
 
     }
     @Override
@@ -44,10 +79,10 @@ public class SecondActivity extends AppCompatActivity {
                 return true;
             case R.id.settings:
                 Toast.makeText(getApplicationContext(), "Help", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(this, Help.class));
+                startActivity(new Intent(this, Settings.class));
                 return true;
             case R.id.logout:
-                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(this, MainActivity.class));
                 return true;
             default:
