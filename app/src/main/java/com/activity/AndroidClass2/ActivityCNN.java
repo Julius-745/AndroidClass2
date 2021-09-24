b9a05b4ff2c9ecc2dcfc0da59ea08eeda17e5cde
hezkia180116 <hezkiavialen@gmail.com>
2021-09-24 18:51:22 +0700

    Upload

---
+++ app/src/main/java/com/activity/AndroidClass2/ActivityCNN.java
@@ -0,0 +1,20 @@
+package com.activity.AndroidClass2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ActivityCNN extends AppCompatActivity {
+public ArticlesFragment fr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cnn);
+        fr.
+
+    }
+}
